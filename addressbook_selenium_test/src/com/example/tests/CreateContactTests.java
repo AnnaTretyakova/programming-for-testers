93ce458e955148207c40a4ee98441838892ed347
AnnaTretyakova <anntretwork@gmail.com>
2014-08-04 10:27:54 +0400

    HomeTask_2

---
+++ addressbook_selenium_test/src/com/example/tests/CreateContactTests.java
@@ -0,0 +1,32 @@
+package com.example.tests;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTests extends TestBase {
+
+  @Test
+  public void testUntitled3() throws Exception {
+	openMainPage();
+	initContactCreation();	
+    ContactData contact = new ContactData();
+    contact.firstname = "Anna";
+    contact.lastname = "Tretyakova";
+    contact.address = "Saint-Petersburg, Popova street, 10";
+    contact.homephone = "1478525";
+    contact.mobilephone = "89501472589";
+    contact.workphone = "89501234567";
+    contact.email = "studentanna@rambler.ru" ;
+    contact.email2 = "studentanna@yandex.ru";
+    contact.bday = "1";
+    contact.bmonth = "August";
+    contact.byear = "1988";
+    contact.group = "g 1";
+    contact.address2 = "Saint-Petersburg, Popova street, 10";
+    contact.homephone2 = "1478525";
+	fillContactForm(contact);
+    submitNewContact();
+    returnHomePage();
+  } 
+}
+
+
--- addressbook_selenium_test/src/com/example/tests/TestBase.java
+++ addressbook_selenium_test/src/com/example/tests/TestBase.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
@@ -38,36 +39,11 @@ public class TestBase {
 	    }
 	  }
 	
-	protected void returnGroupPage() {
-		driver.findElement(By.linkText("group page")).click();
-	}
-
-	protected void submitGroupCreation() {
-	    driver.findElement(By.name("submit")).click();
-	}
-
-	protected void fillGroupForm(GroupData group) {
-	    driver.findElement(By.name("group_name")).click();
-	    driver.findElement(By.name("group_name")).clear();
-	    driver.findElement(By.name("group_name")).sendKeys(group.name);
-	    driver.findElement(By.name("group_header")).clear();
-	    driver.findElement(By.name("group_header")).sendKeys(group.header);
-	    driver.findElement(By.name("group_footer")).clear();
-	    driver.findElement(By.name("group_footer")).sendKeys(group.footer);
-	}
-
-	protected void initGroupCreation() {
-	    driver.findElement(By.name("new")).click();
-	}
-
-	protected void gotoGroupsPage() {
-	    driver.findElement(By.linkText("groups")).click();
-	}
-
+	//Common Methods
 	protected void openMainPage() {
 	    driver.get(baseUrl + "/addressbookv4.1.4/");
 	}
-
+	
 	private boolean isElementPresent(By by) {
 	    try {
 	      driver.findElement(by);
@@ -100,5 +76,81 @@ public class TestBase {
 	      acceptNextAlert = true;
 	    }
 	  }
+	
+	//Methods for GroupCreation
+	//#1
+		protected void gotoGroupsPage() {
+		    driver.findElement(By.linkText("groups")).click();
+		}
+		
+	//#2
+	protected void initGroupCreation() {
+	    driver.findElement(By.name("new")).click();
+	}
 
+	//#3
+		protected void fillGroupForm(GroupData group) {
+		    driver.findElement(By.name("group_name")).click();
+		    driver.findElement(By.name("group_name")).clear();
+		    driver.findElement(By.name("group_name")).sendKeys(group.name);
+		    driver.findElement(By.name("group_header")).clear();
+		    driver.findElement(By.name("group_header")).sendKeys(group.header);
+		    driver.findElement(By.name("group_footer")).clear();
+		    driver.findElement(By.name("group_footer")).sendKeys(group.footer);
+		}
+		
+	//#4
+		protected void submitGroupCreation() {
+		    driver.findElement(By.name("submit")).click();
+		}
+		
+	//#5
+		protected void returnGroupPage() {
+			driver.findElement(By.linkText("group page")).click();
+		}
+		
+	//Methods for contact creation
+	//#1
+		protected void initContactCreation() {
+			driver.findElement(By.linkText("add new")).click();
+		}
+
+	//#2
+		protected void fillContactForm(ContactData contact) {
+			driver.findElement(By.name("firstname")).clear();
+		    driver.findElement(By.name("firstname")).sendKeys(contact.firstname);
+		    driver.findElement(By.name("lastname")).clear();
+		    driver.findElement(By.name("lastname")).sendKeys(contact.lastname);
+		    driver.findElement(By.name("address")).clear();
+		    driver.findElement(By.name("address")).sendKeys(contact.address);
+		    driver.findElement(By.name("home")).clear();
+		    driver.findElement(By.name("home")).sendKeys(contact.homephone);
+		    driver.findElement(By.name("mobile")).clear();
+		    driver.findElement(By.name("mobile")).sendKeys(contact.mobilephone);
+		    driver.findElement(By.name("work")).clear();
+		    driver.findElement(By.name("work")).sendKeys(contact.workphone);
+		    driver.findElement(By.name("email")).clear();
+		    driver.findElement(By.name("email")).sendKeys(contact.email);
+		    driver.findElement(By.name("email2")).clear();
+		    driver.findElement(By.name("email2")).sendKeys(contact.email2);
+		    new Select(driver.findElement(By.name("bday"))).selectByVisibleText(contact.bday);
+		    new Select(driver.findElement(By.name("bmonth"))).selectByVisibleText(contact.bmonth);
+		    driver.findElement(By.name("byear")).clear();
+		    driver.findElement(By.name("byear")).sendKeys(contact.byear);
+		    new Select(driver.findElement(By.name("new_group"))).selectByVisibleText(contact.group);
+		    driver.findElement(By.name("address2")).clear();
+		    driver.findElement(By.name("address2")).sendKeys(contact.address2);
+		    driver.findElement(By.name("phone2")).clear();
+		    driver.findElement(By.name("phone2")).sendKeys(contact.homephone2);
+		}
+		
+	//#3
+		protected void submitNewContact() {
+			driver.findElement(By.name("submit")).click();
+		}
+		
+	//#4
+		protected void returnHomePage() {
+			driver.findElement(By.linkText("home page")).click();
+		}
 }
